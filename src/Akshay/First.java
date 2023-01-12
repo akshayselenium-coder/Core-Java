2def97f764a1335e8fb768b1a6a4dce440e69640
Akshay Pandit <akshay1379@gmail.com>
2023-01-12 17:40:47 +0530

    added and commenting

--- src/Akshay/First.java
+++ src/Akshay/First.java
@@ -7,7 +7,8 @@ public class First {
 		
 		int a=20;
 		System.out.println(a);
-                System.out.println("Akshay");
+                //System.out.println("Akshay");
+                System.out.println("a= "+a+" Value ");
 
 	}
 
