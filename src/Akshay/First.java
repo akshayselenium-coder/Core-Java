08aaf789d271e24108481403df209c2ca791b650
Akshay Pandit <akshay1379@gmail.com>
2023-01-12 17:39:04 +0530

    line added

---
+++ src/Akshay/First.java
@@ -0,0 +1,14 @@
+package Akshay;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=20;
+		System.out.println(a);
+                System.out.println("Akshay");
+
+	}
+
+}
