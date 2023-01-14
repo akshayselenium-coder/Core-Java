d010fba3fc216d45d8269b3304afafd2c4a09ad7
Akshay Pandit <akshay1379@gmail.com>
2023-01-14 22:08:05 +0530

    sklfkdl

--- src/Akshay/B.java
+++ src/Akshay/B.java
@@ -4,7 +4,7 @@ class B
 
  public static void main(String ag[])
  {
-   System.out.println("Sunil"); int a=9;
+   System.out.println("Channey"); int a=9;
 
    int b=20;
 	 System.out.println("Testing Studio");
